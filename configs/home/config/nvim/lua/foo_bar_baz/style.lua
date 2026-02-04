e39baf7621907395de468a1a31cb8ce4bd20f904
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-03 21:22:24 -0600

    woops

--- configs/home/config/nvim/lua/foo_bar_baz/style.lua
+++
@@ -1,7 +0,0 @@
---catppuccin color scheme with no bg
-vim.cmd[[colorscheme tokyonight-moon]]
---vim.cmd[[colorscheme catppuccin]]
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
