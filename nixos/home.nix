2f0cd9dd6fa62d0002ea3e7bb8f1b6c68a6418b7
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-02-21 22:46:07 -0600

    funny

--- nixos/home.nix
+++ nixos/home.nix
@@ -106,5 +106,107 @@ in
         };
       };
     };
+    # hath ye been bamboozled?
+    xdg.configFile."fastfetch/config.jsonc".text = ''
+      {
+        "logo": {
+          "source": "LFS",
+          "color": {
+            "1": "yellow",
+            "2": "white",
+            "3": "blue"
+          }
+        },
+        "modules": [
+          "title",
+          "separator",
+          {
+              "type": "os",
+              "key": "OS",
+              "keyColor": "blue",
+              "format": "no idea"
+          },
+          {
+            "keyColor": "red",
+            "type": "kernel",
+            "key": "Kernel"
+          },
+          {
+            "type": "memory",
+            "key": "Memory",
+            "keyColor": "green",
+            "percent": {
+                "type": 3,     // Show both percentage number and bar
+                "green": 30,   // Values below 30% in green
+                "yellow": 70   // 30-70% in yellow, >70% in red
+            }
+          },
+          {
+            "type": "uptime",
+            "keyColor": "white"
+          },
+          {
+            "type": "packages",
+            "keyColor": "white",
+            "format": "where the heck are your packages?"
+          },
+          {
+            "type": "shell",
+            "keyColor": "white"
+          },
+          {
+            "type": "wm",
+            "keyColor": "white",
+            "format": "not entirely sure"
+          },
+          {
+            "type": "icons",
+            "keyColor": "white"
+          },
+          {
+            "type": "font",
+            "keyColor": "white"
+          },
+          {
+            "type": "cursor",
+            "keyColor": "white"
+          },
+          {
+            "type": "terminal",
+            "keyColor": "white",
+            "format": "NeoVim?"
+          },
+          {
+            "type": "terminalfont",
+            "keyColor": "white"
+          },
+          {
+            "type": "cpu",
+            "keyColor": "white"
+          },
+          {
+            "type": "gpu",
+            "keyColor": "white"
+          },
+          {
+            "type": "custom",
+            "keyColor": "white",
+            "key": "GPU 3",
+            "format": "2.56 pound of raw quartz"
+          },
+          {
+            "type": "swap",
+            "keyColor": "white"
+          },
+          {
+            "type": "break",
+            "keyColor": "white"
+          },
+          {
+            "type": "colors"
+          }
+        ]
+      }
+    '';
   };
 }
