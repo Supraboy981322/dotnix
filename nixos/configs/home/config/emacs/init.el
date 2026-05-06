3e8ca48eb81dee487f5068ef684d9bb4a6f792ca
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-05-06 16:22:00 -0500

    some emacs config stuff

--- nixos/configs/home/config/emacs/init.el
+++ nixos/configs/home/config/emacs/init.el
@@ -1,18 +1,21 @@
-;;;"me emacs config
-;;;  (mostly just curiosity, at the moment)")
+;me emacs config
+;  (mostly just curiosity, at the moment)
 
 ;remove the annoying bell sound
 (setq
   ring-bell-function 'ignore)
 
+;set the config to a sane directory
 (defvar
   config_dir
   "~/.config/emacs/"
   "emacs config dir")
 
+; TODO: what the hell was this for?
 (setq custom-file
   (concat config_dir "custom.el"))
 
+;packages
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "https://melpa.org/packages/") t)
@@ -28,22 +31,71 @@
 (setq
   use-package-always-ensure t)
 
-;evil mode (vim bindings)
+;evil mode
 (unless (package-installed-p 'evil)
-  (package-install 'evil))
+	(package-install 'evil))
 (require 'evil)
 (evil-mode 1)
 
 ;theme
 (use-package
-  ayu-theme
+  ayu-theme ; TODO: the colors don't match NeoVim for this theme
   :config (load-theme 'ayu-dark t))
+
 ;hide the ui (why is this even present by default?)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (setq
-  inhibit-startup-screen t)
+	inhibit-startup-screen t)
 
 (add-to-list 'default-frame-alist
- '(font . "Cascadia Code NF-12"))
+	'(font . "Cascadia Code NF-12"))
+
+;move the autosave and backup files
+;	 to separate directories
+(setq
+ backup-directory-alist
+ `((".".
+    ,(expand-file-name
+			"tmp/backups/"
+			user-emacs-directory))))
+(setq
+ auto-save-file-name-transforms
+ `((".*"
+    ,(expand-file-name
+	  	"tmp/auto-saves/"
+			user-emacs-directory) t)))
+(make-directory
+	(expand-file-name
+		"tmp/backups"
+		user-emacs-directory) t)
+(make-directory
+	(expand-file-name
+		"tmp/auto-saves/"
+		user-emacs-directory) t)
+
+;line numbers
+(global-display-line-numbers-mode 1)
+(setq
+	display-line-numbers-type
+	'relative)
+
+;fix stupid whitespace behavior
+(global-set-key
+	(kbd "TAB")
+	'self-insert-command)
+(setq-default
+	indent-tabs-mode nil)
+(setq-default
+	tab-width 2)
+;I HATE it when an editor does this
+(electric-indent-mode 0)
+(setq-default
+	evil-maybe-remove-spaces nil)
+
+
+;various "modes"
+(use-package zig-ts-mode
+  :vc ( :url "https://codeberg.org/meow_king/zig-ts-mode"
+				:rev :newest))
