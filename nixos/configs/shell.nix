9454b505613ebe484e64a7d5b736cb27ea31b789
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-06-21 14:37:02 -0500

    remove redundant config stuff

--- nixos/configs/shell.nix
+++ nixos/configs/shell.nix
@@ -113,106 +113,13 @@ in {
     };
 
     interactiveShellInit = ''
-      ${builtins.concatStringsSep "\n" (builtins.map (opt: "shopt -s ${opt}") [
-        "globstar"
-        "extglob"
-      ])}
-    '';
-
-    shellInit = /* sh */ ''
-      #mkdir then cd into it
-      mkcd() {
-        mkdir "$1"
-        cd "$1"
-      }
-      #cd into dir then eza
-      cdls() {
-        cd "$1"
-        ${pkgs.eza}/bin/eza ''${@:2}
-      }
-
-      declare less_config=(
-        mb="\e[1;31m"
-        md="\e[1;31m"
-        me="\e[0m"
-        se="\e[0m"
-        so="\e[1;33;44m"
-        ue="\e[0m"
-        us="\e[4;1;32m"
-        mr="\e[7m"
-        mh="\e[2m"
-        ZN="\e[74m"
-        ZV="\e[75m"
-        ZO="\e[73m"
-        ZW="\e[75m"
-      )
-      for thing in "''${less_config[@]}"; do
-        export LESS_TERMCAP_$thing
-      done
-    '';
-    promptInit = /* sh */ ''
-      __mkps1() {
-        #get return status of previous cmd 
-        local EXIT_CODE="$?"
-        #create empty $PS1
-
-        #define colors
-        local BG_GRAY=$'\033[48;2;75;75;75m'
-        local white=$'\033[38;2;255;255;255m'
-        #local yellow=`tput setaf 221`
-        local yellow=$'\033[38;2;255;216;171m'
-        local grey=`tput setaf 249`
-        #local cyan=`tput setaf 45`
-        local cyan=$'\033[38;2;134;225;252m'
-        local bold=`tput bold`
-        #local red=`tput setaf 1`
-        local red=$'\033[38;2;255;117;127m'
-        #local green=`tput setaf 34`
-        local green=$'\033[38;2;195;232;141m'
-        local reset=`tput sgr0`
-        local green2=`tput setaf 70`
-        local timeStamp="$(date +%I:%M%P)"
-        local prompt_color="$green"
-        local italic=$'\033[3m'
-
-        local p="?"
-        #makes the prompt symbol red if bad return code 
-        if (( $EXIT_CODE )); then
-          prompt_color="$red" ; p="!"
-        fi
-        
-        #get current dir
-        local curDir="$(pwd)"
-        #check if dir is within $HOME
-        if [[ "$curDir" == "$HOME"* ]]; then
-          curDir="''${curDir/$HOME/~}" #replace $HOME with '~'
-        fi
-        
-        #adjust printed dir based on term width 
-        local term_width=$((`tput cols`-1))
-        local pre_line=" ╭[00:00xx] {$SHLVL} --> "
-        local pre_len=''${#pre_line}
-        #length of full first line
-        local total_len=$((''${#curDir}+$pre_len))
-        if [[ $total_len -gt $term_width ]]; then
-          local pre_len_diff=$((term_width-pre_len))
-          #cut-off string and add '...' to beginning 
-            curDir=''${curDir: -$((pre_len_diff-3))}
-            curDir="...$curDir"
-        fi
-
-        # ╭[07:10pm] {1} --> ~
-        # ╰(!):
-        # new PS1
-        PS1=$'\n'
-        PS1+=" %{$italic$cyan%}╭[%{$grey%}"
-        PS1+="$timeStamp%{$cyan%}]%{$reset$italic%}"
-        PS1+=" %{$grey%}{"
-        PS1+="$cyan%}$SHLVL%{$grey%}}%{$reset$italic%}"
-        PS1+=" %{$red%}-->%{$reset$italic%} "
-        PS1+="%{$cyan%}$curDir"$'\n'"%{$italic$cyan%}"
-        PS1+=" ╰(%{$reset$italic$prompt_color%}$p"
-        PS1+="%{$reset$italic$cyan%}):%{$reset%} "
+      ${
+        builtins.concatStringsSep "\n" (
+          builtins.map (opt: "shopt -s ${opt}") [
+            "globstar"
+            "extglob"
+          ]
+        )
       }
     '';
   };
